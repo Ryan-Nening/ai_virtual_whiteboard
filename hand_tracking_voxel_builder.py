34e5d096c9416925d29bc64cbd9a63aa6aa06d09
Ryan-Nening <neningryan10@gmail.com>
2026-03-03 00:16:29 +0800

    initial setup with ursina engine and imports

---
+++ hand_tracking_voxel_builder.py
@@ -0,0 +1,8 @@
+import cv2
+from ursina import Ursina, Button, scene, color, destroy, mouse
+from ursina.prefabs.first_person_controller import FirstPersonController
+import mediapipe
+import math
+
+application_engine = Ursina()
+application_engine.run()
